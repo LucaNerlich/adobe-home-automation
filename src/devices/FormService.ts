43e67744e33dc8bcf638c574bd3a44103f65a354
Luca Nerlich <luca.nerlich@googlemail.com>
2022-06-04 11:58:30 +0200

    extract createSubmit

--- src/devices/FormService.ts
+++ src/devices/FormService.ts
@@ -1,8 +1,18 @@
 import {getRandomID} from '../constants'
 
+// On provider creation submit, the new topic will be added here.
+// Can be used by consumer form, for example.
+export const AVAILABLE_TOPICS: Array<string> = []
+
+function createSubmit(label: string) {
+    const submit = document.createElement('input')
+    submit.setAttribute('type', 'submit')
+    submit.setAttribute('value', label)
+    return submit
+}
+
 export function generateProviderForm(formRoot: HTMLFormElement | null) {
     if (formRoot) {
-
         // topic
         const topicId = getRandomID()
         const topicLabel = document.createElement('label')
@@ -35,10 +45,9 @@ export function generateProviderForm(formRoot: HTMLFormElement | null) {
         numberOption.textContent = 'Number Value'
         strategyInput.appendChild(numberOption)
 
-        // submit
-        const submit = document.createElement('input')
-        submit.setAttribute('type', 'submit')
-        submit.setAttribute('value', 'Add Provider')
+
+        const submit = createSubmit('Add Provider')
+
 
         formRoot.appendChild(topicLabel)
         formRoot.appendChild(topicInput)
@@ -46,11 +55,25 @@ export function generateProviderForm(formRoot: HTMLFormElement | null) {
         formRoot.appendChild(strategyLabel)
         formRoot.appendChild(strategyInput)
         formRoot.appendChild(submit)
+
+        // on submit
+        formRoot.addEventListener('submit', (event) => {
+            event.preventDefault()
+            console.log('todo: create provider')
+        })
     }
 }
 
 export function generateConsumerForm(formRoot: HTMLFormElement | null) {
     if (formRoot) {
 
+        const submit = createSubmit('Add Consumer')
+        formRoot.appendChild(submit)
+
+        // on submit
+        formRoot.addEventListener('submit', (event) => {
+            event.preventDefault()
+            console.log('todo: create consumer')
+        })
     }
 }
