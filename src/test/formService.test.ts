3756425185d0f0b4626d35b7f9c4b5f0ef8333a6
Luca Nerlich <luca.nerlich@googlemail.com>
2022-06-04 19:22:46 +0200

    Update formService.test.ts

--- src/test/formService.test.ts
+++ src/test/formService.test.ts
@@ -2,7 +2,13 @@
  * @jest-environment jsdom
  */
 
-import {createSelectOption, createSubmit} from '../devices/FormService'
+import {
+    createSelectOption,
+    createSubmit,
+    generateConsumerForm,
+    generateProviderForm,
+    NAME_FORM_NAME,
+} from '../devices/FormService'
 
 const label = 'some-label'
 
@@ -20,4 +26,36 @@ test('createSelectOption returns option element', () => {
     expect(option).not.toBe(null)
     expect(option.getAttribute('value')).toBe(label)
     expect(option.textContent).toBe(label)
+})
+
+test('generateProviderForm correctly prepares provider-form', () => {
+    const formRoot = document.createElement('form')
+
+    generateProviderForm(formRoot)
+    expect(formRoot).not.toBe(null)
+    expect(formRoot.childElementCount).not.toBe(0)
+})
+
+test('generateConsumerForm correctly prepares consumer-form', () => {
+    const formRoot = document.createElement('form')
+
+    generateConsumerForm(formRoot)
+    expect(formRoot).not.toBe(null)
+    expect(formRoot.childElementCount).not.toBe(0)
+
+    // test name label
+    const nameLabel = formRoot.querySelector('label')
+    expect(nameLabel).not.toBe(null)
+    expect(nameLabel?.getAttribute('for')).not.toBe(null)
+
+    // test name input
+    const nameInput = formRoot.querySelector('input')
+    expect(nameInput).not.toBe(null)
+    expect(nameInput?.getAttribute('type')).toBe('text')
+    expect(nameInput?.getAttribute('name')).toBe(NAME_FORM_NAME)
+    expect(nameInput?.getAttribute('placeholder')).toBe('Kitchen-Light 1')
+    expect(nameInput?.getAttribute('required')).toBe('true')
+
+    const submit = formRoot.querySelector('input[type=submit]')
+    expect(submit).not.toBe(null)
 })
